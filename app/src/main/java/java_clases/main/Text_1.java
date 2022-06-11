240bc1d691c949bb71fdd16e0e9274a45226c0c6
edgarbg82 <105142206+edgarbg82@users.noreply.github.com>
2022-06-10 22:48:02 -0500

    Add files via upload

---
+++ app/src/main/java/java_clases/main/Text_1.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package universidad_nacional_de_colombia.proyecto_estructuras;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author julia
+ */
+public class Text_1 {
+    
+    
+    public void write(String B){
+        try {
+      FileWriter myWriter = new FileWriter("PruebaAVL.txt");
+      myWriter.write(B);
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    }
+    
+    public void read(){
+
+        try {
+            FileReader fr = new FileReader("PruebaAVL.txt");
+      
+            int i;
+        // Holds true till there is nothing to read
+            while ((i = fr.read()) != -1)
+ 
+            // Print all the content of a file
+            System.out.print((char)i);
+
+        }   catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    
+    public void writeFile(String s){
+        BufferedWriter out = null;
+		try {
+			File file = new File("PruebaAVL.txt");
+			out = new BufferedWriter(new FileWriter(file, true));
+			//for (String s : list) {
+				out.write(s);
+				out.newLine();
+
+			//}
+			out.close();
+		} catch (IOException e) {
+		}
+    }
+}
+
